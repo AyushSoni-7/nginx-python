a33ccaae788a0732f35ce5c9131d067823713162
AyushSoni-7 <ayushsoni47.as@gmail.com>
2021-07-19 18:14:24 +0530

    Working flask app with headers

--- flask-app/views/basic.py
+++ flask-app/views/basic.py
@@ -1,19 +1,35 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+
 
 basic_bp = Blueprint('basic_view', __name__)
 
 
+def headers(metadata):
+    data = {
+        "message": "This is the base page",
+        "host": metadata.get("Host", "NULL"),
+        "forward_for": metadata.get("X-Forwarded-For", "NULL"),
+        "forwarded_proto": metadata.get("X-Forwarded-Proto", "NULL"),
+        "upstream_addr": metadata.get("X-Upstream", "NULL")
+    }
+    return data
+
+
 @basic_bp.route("/", methods=["GET"])
 def base():
-    return "This is the base page", 200
+    data = request.environ
+    retVal = headers(data)
+    return jsonify(retVal), 200
 
 
 @basic_bp.route("/blue", methods=["GET"])
 def blue():
-    return "Welcome to the blue app", 200
-
+    retVal = metadata(request)
+    retVal["message"] = "This is the blue page"
+    return jsonify(retVal), 200
 
 @basic_bp.route("/green", methods=["GET"])
 def green():
-    return "Welcom to the green app", 200
-
+    retVal = metadata(request)
+    retVal["message"] = "This is the green page"
+    return jsonify(retVal), 200
