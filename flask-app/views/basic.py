77ea1d39d8e1d172db0860ea1f56e75a78076102
AyushSoni-7 <ayushsoni47.as@gmail.com>
2021-07-19 20:24:38 +0530

    added nginx webserver functionality

--- flask-app/views/basic.py
+++ flask-app/views/basic.py
@@ -1,35 +1,41 @@
 from flask import Blueprint, jsonify, request
-
+import socket
 
 basic_bp = Blueprint('basic_view', __name__)
 
 
 def headers(metadata):
+    host_name = socket.gethostname()
     data = {
         "message": "This is the base page",
         "host": metadata.get("Host", "NULL"),
         "forward_for": metadata.get("X-Forwarded-For", "NULL"),
         "forwarded_proto": metadata.get("X-Forwarded-Proto", "NULL"),
-        "upstream_addr": metadata.get("X-Upstream", "NULL")
+        "host_name": host_name,
+        "host_ip": socket.gethostbyname(host_name)
     }
     return data
 
 
 @basic_bp.route("/", methods=["GET"])
 def base():
-    data = request.environ
+    data = request.headers
     retVal = headers(data)
-    return jsonify(retVal), 200
+    return retVal, 200
 
 
 @basic_bp.route("/blue", methods=["GET"])
 def blue():
-    retVal = metadata(request)
+    data = request.environ
+    retVal = headers(data)
     retVal["message"] = "This is the blue page"
     return jsonify(retVal), 200
 
+
 @basic_bp.route("/green", methods=["GET"])
 def green():
-    retVal = metadata(request)
+    data = request.environ
+    retVal = headers(data)
     retVal["message"] = "This is the green page"
     return jsonify(retVal), 200
+
